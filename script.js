b58622999dc4cc3f883042d1ced6cb9e25d651b1
Simon Stauffer <simonstauffer@Simons-MacBook-Pro.local>
2018-07-29 14:21:05 +1000

    pop up box added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const BUTTON = document.querySelector(".addBtn");
 const TODOLIST = document.querySelector(".toDoList");
 const INFIELD = document.querySelector("#myInput");
+const PopUpBox = document.querySelector("#myDIV");
+const ClosePop = document.querySelector(".popUp-close");
 
-function add(){
-    const INPUT = document.querySelector("#myInput").value;
+function add(element){
+    const INPUT = element;
     const DIV = document.createElement("div");
 
     DIV.className = "list-container";
@@ -37,8 +39,24 @@ function add(){
 
 }
 
+function popUp(){
+    const INPUT = document.querySelector("#myInput").value;
+    PopUpBox.classList.toggle("open");
+    PopUpBox.classList.toggle("closed");
+    if(INPUT){
+    add(INPUT);
+    }
 
-BUTTON.addEventListener("click", add, false);
+}
+
+function popUpClose(){
+    PopUpBox.classList.toggle("open");
+    PopUpBox.classList.toggle("closed");
+}
+
+ClosePop.addEventListener("click", popUpClose, false);
+BUTTON.addEventListener("click", popUp, false);
+//BUTTON.addEventListener("click", add, false);
 
 
 // Enter Key will add new Item
