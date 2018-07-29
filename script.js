34b94106bd7b9d8c407f973db7a8d05e14b4cf13
Simon St <Simon@Simons-MacBook-Pro-2.local>
2018-07-29 23:53:03 +1000

    updated code

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
-const BUTTON = document.querySelector(".addBtn");
-const TODOLIST = document.querySelector(".toDoList");
-const INFIELD = document.querySelector("#myInput");
-const PopUpBox = document.querySelector("#myDIV");
-const ClosePop = document.querySelector(".popUp-close");
+const BUTTON = document.querySelector('.addBtn');
+const TODOLIST = document.querySelector('.toDoList');
+const INFIELD = document.querySelector('#myInput');
+const PopUpBox = document.querySelector('#myDIV');
+const ClosePop = document.querySelector('.popUp-close');
 
-function add(element){
-    const INPUT = element;
-    const DIV = document.createElement("div");
+function add(element) {
+  const INPUT = element;
+  const DIV = document.createElement('div');
 
-    DIV.className = "list-container";
+  DIV.className = 'list-container';
 
-    const LI = document.createElement("li");
+  const LI = document.createElement('li');
 
-    LI.textContent = INPUT;
-    TODOLIST.appendChild(DIV);
-    DIV.appendChild(LI);
+  LI.textContent = INPUT;
+  TODOLIST.appendChild(DIV);
+  DIV.appendChild(LI);
 
+  const CLOSE = document.createElement('SPAN');
 
-    const CLOSE = document.createElement("SPAN");
+  CLOSE.textContent = '\u00D7';
+  CLOSE.className = 'close';
+  // DIV.classList(CLOSE);
+  DIV.appendChild(CLOSE);
 
-    CLOSE.textContent = "\u00D7";
-    CLOSE.className = "close";
-   // DIV.classList(CLOSE);
-    DIV.appendChild(CLOSE);
+  // Clear input field
+  document.querySelector('#myInput').value = '';
 
-    //Clear input field
-    document.querySelector("#myInput").value = "";
+  CLOSE.onclick = function remove() {
+    this.parentNode.remove(this);
+  };
 
-    CLOSE.onclick = function() {
-        this.parentNode.remove(this);
-    }
-
-    // Strike out list item
-    DIV.onclick = function() {
-        LI.classList.toggle("checked");
-        DIV.classList.toggle("background-checked");
-    }
-
+  // Strike out list item
+  DIV.onclick = function strike() {
+    LI.classList.toggle('checked');
+    DIV.classList.toggle('background-checked');
+  };
 }
 
-function popUp(){
-    const INPUT = document.querySelector("#myInput").value;
-    PopUpBox.classList.toggle("open");
-    PopUpBox.classList.toggle("closed");
-    if(INPUT){
+function popUp() {
+  const INPUT = document.querySelector('#myInput').value;
+  PopUpBox.classList.toggle('open');
+  PopUpBox.classList.toggle('closed');
+  if (INPUT) {
     add(INPUT);
-    }
-
+  }
 }
 
-function popUpClose(){
-    PopUpBox.classList.toggle("open");
-    PopUpBox.classList.toggle("closed");
+function popUpClose() {
+  PopUpBox.classList.toggle('open');
+  PopUpBox.classList.toggle('closed');
 }
 
-ClosePop.addEventListener("click", popUpClose, false);
-BUTTON.addEventListener("click", popUp, false);
-//BUTTON.addEventListener("click", add, false);
-
+ClosePop.addEventListener('click', popUpClose, false);
+BUTTON.addEventListener('click', popUp, false);
+// BUTTON.addEventListener("click", add, false);
 
 // Enter Key will add new Item
-INFIELD.addEventListener('keyup',function(e){
+INFIELD.addEventListener(
+  'keyup',
+  e => {
     if (e.keyCode === 13) {
-        BUTTON.click();
-  }  
-},false);
+      BUTTON.click();
+    }
+  },
+  false
+);
